1c3128061dcb3641187c961231e61dc06543fa6b
Kalypso Homan <catcatmcgee@gmail.com>
2024-02-17 22:19:14 -0600

    Add global variables for availability and zip codes

---
+++ src/lib/config.js
@@ -0,0 +1,13 @@
+export const TREATS_AVAILABLE = true;
+export const ZIP_CODES = [
+    '70112',
+    '70113',
+    '70115',
+    '70116',
+    '70118',
+    '70119',
+    '70124',
+    '70125',
+    '70130',
+    'I dont see my zip.'
+]
