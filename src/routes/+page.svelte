4e6e45ba32b3d7bac6393edd0f3fc01675047b3b
Kalypso Homan <catcatmcgee@gmail.com>
2024-02-18 08:19:55 -0600

    Add todo list and initial address and zip forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
+<!--
+TODO:
+1. pressing button at end of logic sequence triggers confetti 
+2. Pressing what's your zip brings down zipcode input.
+3. Pressing horray brings up address form.
+4. Filling out address form brings up stripe payment.
+5. Back button?
+6. Issue form?
+-->
+
 <script>
     import { TREATS_AVAILABLE , ZIP_CODES } from '$lib/config.js';
-
 
     const messages = [
         "Would you like a delightful little treat?",
@@ -8,26 +17,53 @@
         "What's your zip?",
         "Hooray! Tell us where to bring your delightful little treat!",
         "Your treat should be there in about an hour! Yay!"
-    ]
+    ];
     let currentMessageIndex = 0;
     let currentMessage = messages[currentMessageIndex];
-    let selectedZipCode = '';
+    let displayZipForm = false;
+    let displayAddressForm = false;
+    let address = {
+        name: "",
+        addressLineOne: "",
+        addressLineTwo: "",
+        state: "",
+        zip: ""
+    };
+
     function showNextMessage(){
         // If the treats are not currently available.
         if (!TREATS_AVAILABLE) {
             currentMessage = "Sorry but we can't bring you a treat right now.";
-        // If we cannot deliver to the zip code.
-        } else if (selectedZipCode === 'I dont see my zip') {
-            currentMessage = "Oh no! We can't deliver to your area :c";
+        }
+        //If the user is being asked for a zip code, bring up a zip code menu
+        if (currentMessage === "What's your zip?") {
+            displayZipForm = true;
         }
         // If the treats are available, proceed through the messages
-        currentMessageIndex += 1;
-        currentMessage = messages[currentMessageIndex];
+       if (currentMessageIndex >= 4) {
+            makeConfetti();
+            return;
+       } else {
+            currentMessageIndex += 1;
+            currentMessage = messages[currentMessageIndex];
+       }
+
+    }
+
+    // TODO
+    function makeConfetti() {
+
     }
 </script>
 
-<button on:click={showNextMessage}>{currentMessage}</button>
-
-<style>
-
-</style>
+{#if displayZipForm}
+    <input bind:value={address.zip} />
+{:else if displayAddressForm}
+    <input bind:value={address.name}>
+    <input bind:value={address.addressLineOne}>
+    <input bind:value={address.addressLineTwo}>
+    <input bind:value={address.state}>
+    <input bind:value={address.zip}>
+{:else}
+    <button on:click={showNextMessage}>{currentMessage}</button>
+{/if}
