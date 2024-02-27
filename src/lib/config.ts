00a744a42db0297b01603378d50fd9525168e22a
kalypsohoman <kalypsohoman@gmail.com>
2024-02-27 15:27:50 -0600

    Change files to ts

---
+++ src/lib/config.ts
@@ -0,0 +1,13 @@
+export const TREAT_COUNT: number = 200;
+export const NEXT_DELIVERY_DATE: Date | null = new Date(2024, 2, 3);
+export const ZIP_CODES: Array<string> = [
+    '70112',
+    '70113',
+    '70115',
+    '70116',
+    '70118',
+    '70119',
+    '70124',
+    '70125',
+    '70130'
+]
