47ee0b3e9df32d66547a4bb785b28eb57aa86ba0
kalypsohoman <kalypsohoman@gmail.com>
2024-02-27 16:57:15 -0600

    Add currentView variable

--- src/lib/views.ts
+++ src/lib/views.ts
@@ -1,5 +1,6 @@
-import { writable } from "svelte/store";
+import { writable, derived } from "svelte/store";
 
+// Array of different display options for the user to see as they go through the app
 export const views:Array<string> = [
     "Would you like a delightful little treat?",
     "Let's see if we can bring you one. What neighborhood are you in?",
@@ -10,11 +11,14 @@ export const views:Array<string> = [
     "Your treat should be there in about an hour! Yay!"
 ];
 
+// Array of error messages when the treats aren't available :c
 export const unavailableViews:Array<string> = [
     "Sorry but we can't bring you a treat right now.",
     "Oh no! We can't get to your neighborhood right now :c"
 ]
 
+// Create a viewIndex object with some useful methods for navigating through the views.
+// Uses svelte's native store syntax
 function createViewIndex() {
 	const { subscribe, set, update } = writable(0);
 
@@ -27,3 +31,9 @@ function createViewIndex() {
 }
 
 export const currentViewIndex = createViewIndex();
+
+// Creates a currentView variable that is dependent on currentViewIndex using svelte's store syntax.
+export const currentView = derived(
+    currentViewIndex,
+	($currentViewIndex) => views[$currentViewIndex]
+);
