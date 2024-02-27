e3ee89b535b4a63a3f733d9e4835f55fefd05fc1
kalypsohoman <kalypsohoman@gmail.com>
2024-02-27 15:23:25 -0600

    Create layout file that conditionally displays a back button and return home button

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+
+<script lang=ts>
+    import {views, currentViewIndex} from '$lib';
+
+</script>
+
+{ #if $currentViewIndex === (views.length - 1) }
+    <button class="back-button" on:click={currentViewIndex.reset}>⌂</button>
+{ :else if $currentViewIndex !== 0}
+    <button class="back-button" on:click={currentViewIndex.decrement}>←</button>
+{ /if }
+
+<slot />
