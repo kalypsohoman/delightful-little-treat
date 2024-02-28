5fd134b27fbf97724ca1831e573138fe75e68b8d
kalypsohoman <kalypsohoman@gmail.com>
2024-02-28 13:59:23 -0600

    Add currentViewOverride as an export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 export { default as AddressForm } from './AddressForm.svelte'
 export { default as EmbeddedCheckout } from './EmbeddedCheckout.svelte'
 export { default as ZipForm } from './ZipForm.svelte'
-export { views, unavailableViews, currentViewIndex, currentView } from './views'
+export { views, unavailableViews, currentViewIndex, currentView, currentViewOverride } from './views'
