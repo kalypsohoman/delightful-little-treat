0073f1b20f13d354890463835d907bf29c9d9451
kalypsohoman <kalypsohoman@gmail.com>
2024-03-01 17:40:40 -0600

    Added todo about making confetti eventually

--- src/lib/views/CheckoutConfirmation.svelte
+++ src/lib/views/CheckoutConfirmation.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
     import { InitialView, activeComponent } from "$lib";
+
+    // TODO: Make Confetti
+    function makeConfetti() {
+    }
 </script>
 
 <form>
-    <h2>Hooray! Your treat should get there in about an hour.</h2>
+    <h2>Your treat should get there in about an hour.</h2>
+    <button type="button" on:click={makeConfetti}>Hooray!</button>
     <button type="button" on:click={() => activeComponent.set(InitialView)}>⌂</button>
 </form>
