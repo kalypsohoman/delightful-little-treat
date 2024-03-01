8b0c5b3960e8cb186ef0bb60c92ba4ffec4d38e2
kalypsohoman <kalypsohoman@gmail.com>
2024-03-01 11:31:45 -0600

    Removed deleted view exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
-export { default as AddressForm } from './views/AddressForm.svelte'
-export { default as EmbeddedCheckout } from './views/EmbeddedCheckout.svelte'
-export { default as InitialView } from './views/InitialView.svelte'
-export { default as ZipForm } from './views/ZipForm.svelte'
-export { name, address, state, zip } from './stores/customer'
-export { views, unavailableViews, currentViewIndex, currentView, currentViewOverride, activeComponent } from './stores/views'
+export { default as AddressForm } from './views/AddressForm.svelte';
+export { default as EmbeddedCheckout } from './views/EmbeddedCheckout.svelte';
+export { default as InitialView } from './views/InitialView.svelte';
+export { default as ZipForm } from './views/ZipForm.svelte';
+export { name, address, state, zip } from './stores/customer';
+export { activeComponent } from './stores/views';
