2c7e26b1c11be8adc2913aee76c12b2428eee74f
kalypsohoman <kalypsohoman@gmail.com>
2024-03-01 11:45:15 -0600

    Add more imports from things in lib file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,9 @@
 export { default as AddressForm } from './views/AddressForm.svelte';
+export { default as CheckoutConfirmation } from './views/CheckoutConfirmation.svelte';
+export { default as DontWantATreat } from './views/DontWantATreat.svelte'
 export { default as EmbeddedCheckout } from './views/EmbeddedCheckout.svelte';
 export { default as InitialView } from './views/InitialView.svelte';
 export { default as ZipForm } from './views/ZipForm.svelte';
 export { name, address, state, zip } from './stores/customer';
 export { activeComponent } from './stores/views';
+export { TREAT_COUNT, DELIVERING_TODAY, NEXT_DELIVERY_DATE, ZIP_CODES } from './config'
