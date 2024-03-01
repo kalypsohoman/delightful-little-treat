2ae924ebf35820852b6d4dcbd739fa0df5ddee93
kalypsohoman <kalypsohoman@gmail.com>
2024-03-01 11:36:11 -0600

    Init

---
+++ src/lib/views/CheckoutConfirmation.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import InitialView from "./InitialView.svelte";
+    import { activeComponent } from "$lib";
+</script>
+
+<form>
+    <h2>Hooray! Your treat should get there in about an hour.</h2>
+    <button type="button" on:click={() => activeComponent.set(InitialView)}>⌂</button>
+</form>
---
+++ src/lib/views/DontWantATreat.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import InitialView from "./InitialView.svelte";
+    import { activeComponent } from "$lib";
+</script>
+
+<form>
+    <h2>Well, come back when you want a little treat.</h2>
+    <button type="button" on:click={() => activeComponent.set(InitialView)}>↩</button>
+</form>
