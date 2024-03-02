8fdcfac003c1ad2dfdeacf3749bee6040f018c37
kalypsohoman <kalypsohoman@gmail.com>
2024-03-02 12:24:30 -0600

    Remove @ts-nocheck

--- src/lib/views/EmbeddedCheckout.svelte
+++ src/lib/views/EmbeddedCheckout.svelte
@@ -1,43 +1,41 @@
 <script>
-// @ts-nocheck
+  import { onDestroy } from 'svelte'
+  import { register } from '../util'
 
-    import { onDestroy } from 'svelte'
-    import { register } from '../util'
-  
-    /** @type {import('@stripe/stripe-js').Stripe?} */
-    export let stripe
-  
-    /** @type {string?} */
-    export let clientSecret
-  
-    /**
+  /** @type {import('@stripe/stripe-js').Stripe?} */
+  export let stripe
+
+  /** @type {string?} */
+  export let clientSecret
+
+  /**
 	 * @type {string | HTMLElement}
 	 */
-    let wrapper
-  
-    /**
+  let wrapper
+
+  /**
 	 * @type {import("@stripe/stripe-js").StripeEmbeddedCheckout}
 	 */
-    let checkoutElement
-  
-    $: if (stripe) {
-      register(stripe)
-    }
-  
-    $: if (stripe && clientSecret && wrapper) {
-      stripe
-        .initEmbeddedCheckout({ clientSecret })
-        .then((element) => {
-          checkoutElement = element
-          checkoutElement.mount(wrapper)
-        })
-    }
-  
-    onDestroy(() => {
-      checkoutElement?.destroy()
-    });
-  </script>
-  
-  {#if stripe && clientSecret}
-    <div bind:this={wrapper}/>
-  {/if}
+  let checkoutElement
+
+  $: if (stripe) {
+    register(stripe)
+  }
+
+  $: if (stripe && clientSecret && wrapper) {
+    stripe
+      .initEmbeddedCheckout({ clientSecret })
+      .then((element) => {
+        checkoutElement = element
+        checkoutElement.mount(wrapper)
+      })
+  }
+
+  onDestroy(() => {
+    checkoutElement?.destroy()
+  });
+</script>
+
+{#if stripe && clientSecret}
+  <div bind:this={wrapper}/>
+{/if}
