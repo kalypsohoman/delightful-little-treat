96a2d8cfabfe680704194bf0578357af6e522091
kalypsohoman <kalypsohoman@gmail.com>
2024-03-02 13:41:58 -0600

    Remove page from layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,5 @@
     import { fly } from 'svelte/transition';
 </script>
 
-<div 
-  class="page"
-  in:fly={{ y: 200, duration: 500 }}
-  out:fly={{ y: -200, duration: 500 }}
-    >
-  <slot />
-</div>
+
+<slot />
