6d0cde83662ee31ff18405acdaea73eebb85a6da
kalypsohoman <kalypsohoman@gmail.com>
2024-03-04 08:19:40 -0600

    Add noTreatClicked import and rework button to set boolean accordingly

--- src/lib/views/InitialView.svelte
+++ src/lib/views/InitialView.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { DontWantATreat, ZipForm, views } from "$lib";
+    import { DontWantATreat, ZipForm, views, noTreatClicked } from "$lib";
 </script>
 
 <div></div>
@@ -7,9 +7,12 @@
 <form>
     <h2>Would you like a delightful little treat?</h2>
     <row>
-        <button type="button" on:click={() => views.push({ id: "ZipForm", component: ZipForm, props: null})}
+        <button type="button" 
+            on:click={() => {
+                noTreatClicked.set(false);
+                views.push({ id: "ZipForm", component: ZipForm, props: null});
+            }}
             >Yes</button>
-        <button type="button" on:click={() =>  views.push({ id: "DontWantATreat", component: DontWantATreat, props: null})}
-            >No</button>
+        <button type="button" on:click={() => {noTreatClicked.set(true)}}>No</button>
     </row>
 </form>
