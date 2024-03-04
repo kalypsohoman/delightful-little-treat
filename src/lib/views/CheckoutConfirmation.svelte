aab85ec0df12bab799cc287d2a41f8849ba3a6b9
kalypsohoman <kalypsohoman@gmail.com>
2024-03-04 15:49:31 -0600

    Add confetti using svelte-confetti library

--- src/lib/views/CheckoutConfirmation.svelte
+++ src/lib/views/CheckoutConfirmation.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import { views } from "$lib";
-
-    // TODO: Make Confetti
-    function makeConfetti() {
-    }
+    import { Confetti } from "svelte-confetti"
+    import ToggleConfetti from "./ToggleConfetti.svelte";
 </script>
 
 <form>
     <h2>Your treat should get there in about an hour.</h2>
-    <button type="button" on:click={ makeConfetti }>Hooray!</button>
-    <button type="button" on:click={ views.reset }>⌂</button>
+    <ToggleConfetti>
+        <button type="button" slot="label">Hooray!</button>
+        <Confetti y={[-0.5, 1]} x={[-1, 1]} delay={[100, 200]} duration={2500} amount={50} />
+    </ToggleConfetti>
+    <button type="button" on:click={ views.reset }>Go back to the start</button>
 </form>
