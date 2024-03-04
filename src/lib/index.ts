a2fd9ae7c3a973e430578c463ec5f87c82eea152
kalypsohoman <kalypsohoman@gmail.com>
2024-03-04 12:55:53 -0600

    Replace EmbeddedCheckout with StripeCheckout

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,8 @@
 export { default as AddressForm } from './views/AddressForm.svelte';
 export { default as CheckoutConfirmation } from './views/CheckoutConfirmation.svelte';
 export { default as DontWantATreat } from './views/DontWantATreat.svelte'
-export { default as EmbeddedCheckout } from './views/EmbeddedCheckout.svelte';
 export { default as InitialView } from './views/InitialView.svelte';
+export { default as StripeCheckout } from './views/StripeCheckout.svelte';
 export { default as ZipForm } from './views/ZipForm.svelte';
 export { name, address, state, zip } from './stores/customer';
 export { views, noTreatClicked } from './stores/views';
