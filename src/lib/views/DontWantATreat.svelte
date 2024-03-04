ff958ca2aa6596bd4a39f5c65cb9d0562d5a7c18
kalypsohoman <kalypsohoman@gmail.com>
2024-03-04 08:19:21 -0600

    Add noTreatClicked import and rework button to set value to false

--- src/lib/views/DontWantATreat.svelte
+++ src/lib/views/DontWantATreat.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import { views } from "$lib";
+    import { views, noTreatClicked } from "$lib";
 </script>
 
 <form>
     <h2>Well, come back when you want a little treat.</h2>
-    <button type="button" on:click={views.reset}>↩</button>
+    <button type="button" on:click={() => {noTreatClicked.set(false); views.reset}}>↩</button>
 </form>
