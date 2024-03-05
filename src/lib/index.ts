d84740a412ed35e9d5df8b8ffd2b3cf4f4d92577
kalypsohoman <kalypsohoman@gmail.com>
2024-03-05 13:43:32 -0600

    Add canSeeCheckoutConfirmation export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,6 @@ export { default as InitialView } from './views/InitialView.svelte';
 export { default as StripeCheckout } from './views/StripeCheckout.svelte';
 export { default as ZipForm } from './views/ZipForm.svelte';
 export { name, address, state, zip } from './stores/customer';
-export { views, noTreatClicked } from './stores/views';
+export { views, noTreatClicked, canSeeCheckoutConfirmation } from './stores/views';
 export type { View } from './stores/views';
 export { TREAT_COUNT, DELIVERING_TODAY, NEXT_DELIVERY_DATE, ZIP_CODES } from './config'
